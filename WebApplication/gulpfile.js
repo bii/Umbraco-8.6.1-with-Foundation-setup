650bf667c56033589762cf799bce83f2f5c6d2a8
Bilal Isa <bilal.isa@pm.me>
2020-04-24 20:34:54 +0200

    master first commit

---
+++ WebApplication/gulpfile.js
@@ -0,0 +1,37 @@
+﻿const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(concat('all.scss'))
+        .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', gulp.task('sass'));
+});
+
+gulp.task('default', () =>
+    gulp.src('./src/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/env'],
+            compact: true,
+            minified: true
+        }))
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist'))
+);
+
+gulp.task('default:watch', function () {
+    gulp.watch('./src/**/*.js', gulp.task('default'));
+});
+
+gulp.task('all:watch', function () {
+    gulp.watch('./src/**/*.js', gulp.task('default'));
+    gulp.watch('./sass/**/*.scss', gulp.task('sass'));
+});
